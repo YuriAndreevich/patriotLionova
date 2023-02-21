69b3a305f1746c19bf22b8b1ffcb1e136ed85754
yuriandreevich <YuriAndreev11ch@gmail.com>
2023-02-21 13:50:45 +0300

    1

--- src/img/i1/data.js
+++ src/img/i1/data.js
@@ -2,6 +2,14 @@ import i1 from './1.jpg'
 import i2 from './2.jpg'
 import i3 from './3.jpg'
 import i4 from './4.jpg'
+import i5 from './4.jpg'
+import i6 from './4.jpg'
+import i7 from './4.jpg'
+import i8 from './4.jpg'
+import i9 from './4.jpg'
+import i10 from './4.jpg'
+import i11 from './4.jpg'
+import i12 from './4.jpg'
 
 
 const data = [
@@ -9,6 +17,14 @@ const data = [
     { src: i2 },
     { src: i3 },
     { src: i4 },
+    { src: i5 },
+    { src: i6 },
+    { src: i7 },
+    { src: i8 },
+    { src: i9 },
+    { src: i10 },
+    { src: i11 },
+    { src: i12 },
 
 ]
 export default data
